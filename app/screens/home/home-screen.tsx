10e255705fc7e6782121962d5e21994068d88209
Guy Ariely <guyariely@gmail.com>
2021-08-02 12:45:50 +0300

    added sign out

--- app/screens/home/home-screen.tsx
+++ app/screens/home/home-screen.tsx
@@ -1,17 +1,37 @@
 import React from "react";
-import { View, ViewStyle } from "react-native";
+import {
+  View,
+  ViewStyle,
+  TextStyle,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import TodoList from "../../components/todo-list/todo-list";
 import Heading from "../../components/heading/heading";
 import NewTodoForm from "../../components/new-todo-form/new-todo-form";
 import FiltersForm from "../../components/filters-form/filters-form";
 import Screen from "../../components/screen/screen";
+import { signOut } from "../../actions/auth/auth";
+import { connect } from "react-redux";
+import { spacing } from "../../theme";
 
 const CONTAINER: ViewStyle = { flex: 1 };
 
-function HomeScreen() {
+const LOGOUT_BUTTON: TextStyle = {
+  paddingHorizontal: spacing[5],
+};
+
+const LOGOUT_BUTTON_TEXT: TextStyle = {
+  fontSize: spacing[6],
+};
+
+function HomeScreen(props: any) {
   return (
     <Screen>
       <View style={CONTAINER}>
+        <TouchableOpacity style={LOGOUT_BUTTON} onPress={() => props.signOut()}>
+          <Text style={LOGOUT_BUTTON_TEXT}>{"👋🏻"}</Text>
+        </TouchableOpacity>
         <Heading text="What's up, Joy!" />
         <FiltersForm />
         <NewTodoForm />
@@ -21,4 +41,8 @@ function HomeScreen() {
   );
 }
 
-export default HomeScreen;
+const mapDispatchToProps = (dispatch: any) => ({
+  signOut: () => dispatch(signOut()),
+});
+
+export default connect(undefined, mapDispatchToProps)(HomeScreen);
